c803db7c817cacc68a8678e9c587c09a3cbf8ea0
zeenu <a.virli@hotmail.com>
2022-11-01 18:03:02 +0000

    updated css & html

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-# File Metadata Microservice
+### File Metadata Microservice - A Backend Project
+In software development Micro-services are simply an organisational approach where an applications is made up of small independent services that communicate over well-defined APIs.
+ 
+ They are also known as Microservice Architecture. The biggest advantage is that they are loosely coupled so if something is broken we can easily pinpoint faults and fix things, but also if we make small changes it won't end up breaking the entire app.
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+
+<br>
+
+
+### Boilerplate
+```
+Boilerplate code used from FreeCodeCamp at: 
+
+
+```
+
+
+<br>
+
+### Dependencies
+```
+cors
+express
+nodemon
+dotenv
+multer
+
+```
+
+<br>
+
+### Getting Started
+```
+npm i cors express nodemon dotenv multer
+npm start 
+
+```
+
+<br>
+
+### Inside package.json...
+```
+  "scripts": {
+    "start": "nodemon index.js"
+```
+
+<br>
+
+### Demo
+https://timestamp.zeenux0.repl.co/
+
+![My Image](img/preview.png)
+
+
+
--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const dotenv = require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require("multer");
+const app = express();
+const upload = multer({dest: 'uploads/'})
 
-var app = express();
+
+//MIDDLEWARE
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+
+//Serve the request with index.html, object.method(path, handler)
+app.get('/', function(req,res){
+  const filePath =__dirname + '/views/index.html';
+    res.sendFile(filePath);
+  });
+
+
+//app.use(path, middlewareFunction), Gives us access to static assets like Style.css
+const style = __dirname + '/public';
+app.use('/public', express.static(style));
+
+
+
+//Upload file and check success, upfile specified in index.html
+app.post('/api/fileupload', upload.single('upfile'), function (req, res) {
+  console.log(req.file)
+  console.log(req.body)
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype, 
+    size: req.file.size
+  })
+})
+
+
+
+//Giving Port Access:
+//Listen for requests - PORT specified in .env shell file
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Node app is listening at http://localhost:' + listener.address().port );
 });
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
